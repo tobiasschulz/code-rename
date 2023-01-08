fc6fb2616a818d7e299cdbe0e0e3f7ad5ba11bb3
Tobias Schulz <tobias.schulz@linux.com>
2023-01-08 14:34:39 +0100

    Update coderename.pl

--- coderename.pl
+++ coderename.pl
@@ -1,11 +1,41 @@
 #!/usr/bin/perl
 
-$numArgs = $#ARGV + 1;
-print "thanks, you gave me $numArgs command-line arguments:\n";
+use strict;
+use List::Permutor;
 
-foreach $argnum (0 .. $#ARGV) {
-    print "$ARGV[$argnum]\n";
+if ( $#ARGV + 1 != 2 ) {
+    print "Wrong count of command line arguments: $#ARGV.\n";
+    exit(1);
 }
 
+my ( $words_before_str, $words_after_str ) = @ARGV;
 
+my @words_before = split /\s+/, $words_before_str;
+my @words_after  = split /\s+/, $words_after_str;
+
+my $count_words_before = $#words_before + 1;
+print "Count of words: $count_words_before:\n";
+
+foreach my $word_index ( 0 .. $#words_before ) {
+    print "- $words_before[$word_index] => $words_after[$word_index]\n";
+}
+
+print "\n";
+
+my @replacements = ();
+
+foreach my $word_index ( 0 .. $#words_before ) {
+
+    my $separator_permutor = List::Permutor->new( '-', '_', '.', '/', '\\', '', );
+    while ( my @separator_permutation = $separator_permutor->next() ) {
+
+        print "@separator_permutation\n";
+        my $s = '';
+        foreach my $word_index_inner ( 0 .. $word_index ) {
+
+        }
+
+    }
+
+}
 
